00b0623f38296047e8009d22e52cc4d161b5df2e
cle4r-gif <tim0610@snu.ac.kr>
2025-02-28 10:28:54 +0900

    improve selenium(제발)

--- module.py
+++ module.py
@@ -275,11 +275,15 @@ def scrape_insta(username, password, url_lst, artist_lst):
     options = webdriver.ChromeOptions()
     options.add_argument("--lang=en")
     options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")  # 창 크기 지정
+    options.add_argument("--disable-gpu")  # GPU 비활성화 (Headless 안정성 증가)
+    options.add_argument("--disable-dev-shm-usage")  # 메모리 부족 문제 해결
     options.add_argument('--no-sandbox')
     options.add_argument("--log-level=3")
-    mobile_emulation = {
-        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
-    options.add_experimental_option("mobileEmulation", mobile_emulation)
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+    # mobile_emulation = {
+    #     "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
+    # options.add_experimental_option("mobileEmulation", mobile_emulation)
 
 
     bot = webdriver.Chrome(service=service, options=options)
@@ -365,11 +369,15 @@ def scrape_X(username, password, url_lst, artist_lst):
     options = webdriver.ChromeOptions()
     options.add_argument("--lang=en")
     options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")  # 창 크기 지정
+    options.add_argument("--disable-gpu")  # GPU 비활성화 (Headless 안정성 증가)
+    options.add_argument("--disable-dev-shm-usage")  # 메모리 부족 문제 해결
     options.add_argument('--no-sandbox')
     options.add_argument("--log-level=3")
-    mobile_emulation = {
-        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
-    options.add_experimental_option("mobileEmulation", mobile_emulation)
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+    # mobile_emulation = {
+        # "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
+    # options.add_experimental_option("mobileEmulation", mobile_emulation)
 
     bot = webdriver.Chrome(service=service, options=options)
     bot.set_page_load_timeout(15)  # Set the page load timeout to 15 seconds
